9decdc661272beedadb58f2e0820b5a49286b7cb
yomZsamora <samora.y@adzumi.co.ke>
2022-02-06 18:27:55 +0300

    Adding event listener to capture form data and override a form's default behavior.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const init = () => {
-  
+   const inputForm = document.querySelector('form');
+   inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+   });
 }
 
 document.addEventListener('DOMContentLoaded', init);
